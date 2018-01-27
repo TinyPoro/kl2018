c4aaad662a5ea189701cbba06def27d18ec3f38c
TinyPoro <ngophuongtuan@gmail.com>
2018-01-28 00:55:26 +0700

    sixth day

--- app/Console/Commands/CrawlTest.php
+++ app/Console/Commands/CrawlTest.php
@@ -2,7 +2,13 @@
 
 namespace App\Console\Commands;
 
+use App\Article;
+use App\Comment;
+use App\HostRule;
 use Illuminate\Console\Command;
+use Openbuildings\Spiderling\Driver_Phantomjs;
+use Openbuildings\Spiderling\Driver_Phantomjs_Connection;
+use Openbuildings\Spiderling\Page;
 
 class CrawlTest extends Command
 {
@@ -11,7 +17,8 @@ class CrawlTest extends Command
      *
      * @var string
      */
-    protected $signature = 'test:crawl';
+    protected $signature = 'test:crawl
+    {--article=308 : article id to run}';
 
     /**
      * The console command description.
@@ -19,6 +26,9 @@ class CrawlTest extends Command
      * @var string
      */
     protected $description = 'Test crawl';
+
+    private $date_pattern = '/\d+\/\d+\/\d+/u';
+    private $id_pattern = '/(?<=-)\d+(?=\.htm)/ui';
 
     /**
      * Create a new command instance.
@@ -37,6 +47,112 @@ class CrawlTest extends Command
      */
     public function handle()
     {
-        //
+        $article_id = $this->option('article');
+        $article = Article::find($article_id);
+        if(!$article) $article = new Article();
+
+        $host = $article->host;
+        $url = $article->url;
+
+        preg_match($this->id_pattern, $url, $id_matches);
+        $id = $id_matches['0'];
+
+        $phantomjs_driver_connection = new Driver_Phantomjs_Connection('http://localhost');
+        $phantomjs_driver_connection->port(4445);
+        $phantomjs_driver = new Driver_Phantomjs();
+        $phantomjs_driver->connection($phantomjs_driver_connection);
+
+        $page = new Page($phantomjs_driver);
+
+        $page->visit($url);
+
+        //title
+        $article->title = $page->find('title')->text();
+
+        $rules = HostRule::where('host', $host)->first();
+
+//        //date
+        $date_rules = json_decode($rules->date_rule, TRUE);
+        $date_text = "";
+        foreach ($date_rules as $date_rule){
+            list($type, $selector) = explode(": ", $date_rule, 2);
+
+            if($type == 'css'){
+                $date = $page->find($selector);
+
+                preg_match($this->date_pattern, $date->text(), $date_matches);
+                $date_text = $date_matches['0'];
+            }
+        }
+
+        $date_obj = \DateTime::createFromFormat('d/m/Y', $date_text);
+        $article->date=$date_obj->format('Y-m-d');;
+//
+//        //content
+        $content_rules = json_decode($rules->content_rule, TRUE);
+        $content_text = "";
+        foreach ($content_rules as $content_rule){
+            list($type, $selector) = explode(": ", $content_rule, 2);
+
+            if($type == 'css'){
+                $contents = $page->all($selector);
+                foreach ($contents as $content){
+                    $content_text .= $content->text();
+                }
+            }
+        }
+        $article->content = $content_text;
+
+        $article->save();
+
+        //comment
+        $comment_rules = json_decode($rules->comment_rule, TRUE);
+        foreach ($comment_rules as $comment_rule){
+            list($type, $selector) = explode(": ", $comment_rule, 2);
+
+            if($type == 'css-name'){
+                $contents = $page->all($selector);
+
+                foreach ($contents as $content){
+                    $comment = Comment::where('article_id', $article_id)
+                        ->whereNull('user_name')
+                        ->orderBy('id', 'asc')
+                        ->first();
+
+                    $comment->user_name = $content->text();
+                    $comment->save();
+                }
+            }
+
+            if($type == 'css-content'){
+                Comment::where('article_id', $article_id)->delete();
+                $contents = $page->all($selector);
+
+                foreach ($contents as $content){
+                    $comment = new Comment();
+                    $comment->article_id = $article_id;
+                    $comment->content = $content->text();
+                    $comment->save();
+                }
+            }
+
+            if($type == 'visit'){
+                try{
+                    $target = $page->find($selector);
+                }
+                catch (\Exception $e){
+                    echo $e->getMessage()."\n";
+                    break;
+                }
+
+                $page->visit($target->attribute('src'));echo "visit $target->attribute('src')\n";
+            }
+
+            if($type == 'visit_id'){
+                $target_url = $selector. $id;
+
+                $page->visit($target_url); echo "visit $target_url\n";
+            }
+        }
     }
 }
